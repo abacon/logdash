e9f5542808ee0419d971723aa3fe10f645c01b16
Andrew Bacon <Andrew Bacon abacon@gmail.com>
2014-01-30 23:26:51 -0500

    Initialized repo

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('logdashApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
